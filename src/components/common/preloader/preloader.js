dc194aa7ddd9d53ba607eb22fd3269e0b4216c7d
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2022-07-12 13:06:31 +0300

    upd 12/07/22

---
+++ src/components/common/preloader/preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import loader from "../../../assets/images/loader.gif";
+
+
+
+let Preloader=(props) => {
+    return  <img src={loader}/>
+}
+
+export default Preloader
